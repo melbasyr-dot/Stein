01e683655a8e551977a0cf817705ba0d97f47a18
melbasyr-dot <melbasyr@gmail.com>
2026-06-05 21:55:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function addToCart(product) {
+let total = 0;
+
+function addToCart(product, price) {
   let li = document.createElement("li");
-  li.textContent = product + " added";
+
+  li.textContent = product + " - " + price + " MAD ";
+
+  let btn = document.createElement("button");
+  btn.textContent = "Remove";
+  btn.onclick = function() {
+    total -= price;
+    document.getElementById("total").textContent = total;
+    li.remove();
+  };
+
+  li.appendChild(btn);
   document.getElementById("cart").appendChild(li);
+
+  total += price;
+  document.getElementById("total").textContent = total;
 }
