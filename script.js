00e1b4679c27b5ed295c5e37ea3e0b2fe6f815f3
melbasyr-dot <melbasyr@gmail.com>
2026-06-05 22:00:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let total = 0;
 
-function addToCart(product, price) {
+function addToCart(name, price) {
   let li = document.createElement("li");
-
-  li.textContent = product + " - " + price + " MAD ";
+  li.textContent = name + " - " + price + " MAD ";
 
   let btn = document.createElement("button");
   btn.textContent = "Remove";
-  btn.onclick = function() {
+
+  btn.onclick = function () {
     total -= price;
     document.getElementById("total").textContent = total;
     li.remove();
@@ -18,4 +18,31 @@ function addToCart(product, price) {
 
   total += price;
   document.getElementById("total").textContent = total;
+}
+
+/* SEARCH */
+function searchProducts() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let cards = document.getElementsByClassName("card");
+
+  for (let card of cards) {
+    card.style.display = card.textContent.toLowerCase().includes(input)
+      ? "block"
+      : "none";
+  }
+}
+
+/* FILTER */
+function filterCategory(cat) {
+  let cards = document.getElementsByClassName("card");
+
+  for (let card of cards) {
+    if (cat === "all") {
+      card.style.display = "block";
+    } else {
+      card.style.display = card.classList.contains(cat)
+        ? "block"
+        : "none";
+    }
+  }
 }
